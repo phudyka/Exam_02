1eb8c7f0e081fabc98b851dcc7eb2b28ecbd1eff
phudyka <paul.hudyka@gmail.com>
2023-03-28 11:34:07 +0200

    #include <stddef.h>

--- Level_2/ft_strcspn/ft_strcspn.c
+++ Level_2/ft_strcspn/ft_strcspn.c
@@ -1,18 +1,20 @@
-#include <stdio.h>
+#include <stddef.h>
 
-size_t	ft_strcspn(const char *s, const char *reject)
+size_t ft_strcspn(const char *s, const char *reject)
 {
-	size_t	i, j = 0;
+    size_t i = 0;
 
-	while (s[i])
-	{
-		while (reject[j])
-		{
-			if (s[i] == reject[j])
-				return(i);
-			j++;
-		}
-		i++;
-	}
-	return (i);
+    while (s[i])
+    {
+        size_t j = 0;
+        while (reject[j])
+        {
+            if (s[i] == reject[j])
+                return i;
+            j++;
+        }
+        i++;
+    }
+    return i;
 }
+
