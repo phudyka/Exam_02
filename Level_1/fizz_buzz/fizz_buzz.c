235dbe5ba867f93198c042800d834add9fc8e14c
phudyka <paul.hudyka@gmail.com>
2023-03-29 12:18:59 +0200

    fixed if statements causing 'fizzbuzz' never showing

--- Level_1/fizz_buzz/fizz_buzz.c
+++ Level_1/fizz_buzz/fizz_buzz.c
@@ -2,31 +2,37 @@
 
 void ft_putnbr(int nbr)
 {
-	char *str = "0123456789";
+   char *str = "0123456789";
 
-	if (nbr > 9)
-		ft_putnbr(nbr / 10);
-	write(1, &str[nbr % 10], 1);
+   if (nbr >= 10)
+      ft_putnbr(nbr / 10);
+   write(1, &str[nbr % 10], 1);
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
-	int i = 1;
+   (void)argv;
+   if (argc == 1)
+   {
+      int i = 1;
 
-	while (i <= 100)
-	{
-		if ((i % 3) == 0)
-			write(1, "fizz", 5);
-		else if ((i % 5) == 0)
-			write(1, "buzz", 5);
-		else if (((i % 3) == 0) && ((i % 5) == 0))
-			write(1, "fizzbuzz", 9);
-		else
-			ft_putnbr(i);
-		write(1, "\n", 1);
-		i++;
-	}
-	else
-		write(1, "\n", 1);
-	return 0;
+      while (i <= 100)
+      {
+         if (((i % 3) == 0) && ((i % 5) == 0))
+            write(1, "fizzbuzz\n", 9);
+         else if ((i % 3) == 0)
+            write(1, "fizz\n", 5);
+         else if ((i % 5) == 0)
+            write(1, "buzz\n", 5); 
+         else
+         {
+            ft_putnbr(i);
+            write(1, "\n", 1);
+         }
+         i++;
+      }
+   }
+   else
+      write(1, "\n", 1);
+   return 0;
 }
