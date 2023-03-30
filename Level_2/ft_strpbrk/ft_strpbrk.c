6c53957fd3aa3587a86a1d1445a33b2a079a9b76
Paul Hudyka <phudyka@c2r9p1.42nice.fr>
2023-03-30 09:50:05 +0200

    ok

--- Level_2/ft_strpbrk/ft_strpbrk.c
+++ Level_2/ft_strpbrk/ft_strpbrk.c
@@ -1,13 +1,15 @@
 char *ft_strpbrk(const char *s1, const char *s2)
 {
-	int i,j = 0;
+	int i = 0;
+	int j;
 
 	while (s1[i])
 	{
+		j = 0;
 		while (s2[j])
 		{
-			if (s1[i] == s2[i])
-				return((char *)s1);
+			if (s1[i] == s2[j])
+				return((char *)s1 + i);
 			j++;
 		}
 		i++;
