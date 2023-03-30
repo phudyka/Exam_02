c307b4a9136b15c7a4e3374da86398d7e9544ee1
Paul Hudyka <phudyka@c2r9p1.42nice.fr>
2023-03-30 09:12:21 +0200

    ok

--- Level_1/repeat_alpha/repeat_alpha.c
+++ Level_1/repeat_alpha/repeat_alpha.c
@@ -2,26 +2,35 @@
 
 int main(int argc, char **argv)
 {
-	if (argc == 2)
-	{
-		int i, j = 0;
+    if (argc == 2)
+    {
+        int i = 0;
+        int j = 0;
 
-		while (argv[1][i])
-		{
-			if ((argv[1][i] >= 'a' && argv[1][i] <= 'z') || (argv[1][i] >= 'A' && argv[1][i] <= 'Z'))
-			{
-				j = 0;
-				while (j < argv[1][i] - 'A' + 1 || j < argv[1][i] - 'a' + 1)
+        while (argv[1][i])
+        {
+			j = 0;
+            if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
+            {
+                while (j < argv[1][i] - 'a' + 1)
 				{
-					write(1, &argv[1][i], 1);
+                    write(1, &argv[1][i], 1);
 					j++;
 				}
-			}
-			else
-				write(1, &argv[1][i], 1);
-			i++;
-		}
-	}
-	write(1, "\n", 1);
-	return 0;
-}
+            }
+            else if ( argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+            {
+                while (j < argv[1][i] - 'A' + 1)
+				{
+                    write(1, &argv[1][i], 1);
+					j++;
+				}
+            }
+            else
+                write(1, &argv[1][i], 1);
+            i++;
+        }
+    }
+    write(1, "\n", 1);
+    return 0;
+}	
